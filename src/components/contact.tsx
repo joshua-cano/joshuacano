eaa44b1183ebb36cb4b8404a4cab96205728e904
Joshua Cano <joshuacano@unknownbe0b31d5e2c1.attlocal.net>
2024-11-11 20:19:00 -0600

    Update Contact component

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -4,7 +4,7 @@ export const Contact = () => {
   return (
     <div className="mx-auto max-w-screen-lg space-y-8 px-8 py-8">
       <h2 className="max-w-2xl text-3xl font-semibold">
-        Like what you see? Reach out via email to collaborate!
+        Like what you see? Reach out via email!
       </h2>
       <Button asChild>
         <a href="mailto:joshua@joshuacano.com">Email Me</a>
